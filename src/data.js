47af71e31d2ccf8913c9988001dd72fde0e701be
Alexandra Dumitrescu <89261015+DumitrescuAlexandra@users.noreply.github.com>
2022-03-17 17:29:08 +0200

    Update data.js with bitly

--- src/data.js
+++ src/data.js
@@ -9,25 +9,25 @@ export const projects = [
   {
     id: 2,
     img: "https://user-images.githubusercontent.com/89261015/155707448-bca88e78-6582-4ab1-96bd-c0291ca1caf6.png",
-    link: "https://alexandra-omnifood.netlify.app",
+    link: "bit.ly/3Ij3pmS",
     description: "HTML / CSS",
   },
   {
     id: 3,
     img: "https://user-images.githubusercontent.com/89261015/155708993-684541fc-493f-418a-93fe-7a2a7c6fba55.png",
-    link: "https://the-crib.netlify.app",
+    link: "bit.ly/3thS7La",
     description: "HTML / CSS / SCSS",
   },
   {
     id: 4,
     img: "https://user-images.githubusercontent.com/89261015/155716117-8692bb76-d2cf-4658-b689-62c84e08a7f0.png",
-    link: "https://add-to-chart-app.netlify.app",
+    link: "bit.ly/3id7qOY",
     description: "ReactJS / context API / Javascript",
   },
   {
     id: 5,
     img: "https://user-images.githubusercontent.com/89261015/155716966-a80fe64d-4d7a-4275-af4e-2b9d7514c57d.png",
-    link: "https://bankist-vanilla-javascript.netlify.app/",
+    link: "bit.ly/3JlKQj8",
     description:
       "Vanilla Javascript / HTML / CSS  *****  JS (user) & 1111 (PIN)",
   },
