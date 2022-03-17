fdc72ef0ffe19c46f7b4cabc83bad8639e039d33
Alexandra Dumitrescu <89261015+DumitrescuAlexandra@users.noreply.github.com>
2022-03-17 17:37:33 +0200

    Updated data.js with https://bitly

--- src/data.js
+++ src/data.js
@@ -2,32 +2,32 @@ export const projects = [
   {
     id: 1,
     img: "https://user-images.githubusercontent.com/89261015/155715049-8bd16771-e568-4d38-bc7e-ebd9681bf11f.png",
-    link: "https://DumitrescuAlexandra.github.io/Psychademy",
+    link: "https://bit.ly/psychademy",
     description:
       "ReactJS  /  Conetxt API  / Modular CSS /  Firebase/Firestore  / Flexbox  / Framer Motion",
   },
   {
     id: 2,
     img: "https://user-images.githubusercontent.com/89261015/155707448-bca88e78-6582-4ab1-96bd-c0291ca1caf6.png",
-    link: "bit.ly/3Ij3pmS",
+    link: "https://bit.ly/3Ij3pmS",
     description: "HTML / CSS",
   },
   {
     id: 3,
     img: "https://user-images.githubusercontent.com/89261015/155708993-684541fc-493f-418a-93fe-7a2a7c6fba55.png",
-    link: "bit.ly/3thS7La",
+    link: "https://bit.ly/3thS7La",
     description: "HTML / CSS / SCSS",
   },
   {
     id: 4,
     img: "https://user-images.githubusercontent.com/89261015/155716117-8692bb76-d2cf-4658-b689-62c84e08a7f0.png",
-    link: "bit.ly/3id7qOY",
+    link: "/bit.ly/3id7qOY",
     description: "ReactJS / context API / Javascript",
   },
   {
     id: 5,
     img: "https://user-images.githubusercontent.com/89261015/155716966-a80fe64d-4d7a-4275-af4e-2b9d7514c57d.png",
-    link: "bit.ly/3JlKQj8",
+    link: "https://bit.ly/3JlKQj8",
     description:
       "Vanilla Javascript / HTML / CSS  *****  JS (user) & 1111 (PIN)",
   },
